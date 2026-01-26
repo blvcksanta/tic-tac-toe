6e696714007c46626eb4ba77445415e7327cad47
Danil <adamjoeblvck@gmail.com>
2026-01-26 20:53:17 +0400

    build:

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,47 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import Autoprefixer from 'autoprefixer'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    svgLoader({
-      svgo: true,
-      svgoConfig: {
-        plugins: [
-          {
-            name: 'prefixIds',
-            params: {
-              delim: '__',
-              prefixIds: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      svgLoader({
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'prefixIds',
+              params: {
+                delim: '__',
+                prefixIds: true,
+              },
             },
-          },
-        ],
+          ],
+        },
+      }),
+    ],
+    css: {
+      transformer: 'lightningcss',
+      postcss: {
+        plugins: [Autoprefixer()],
       },
-    }),
-  ],
-  css: {
-    postcss: {
-      plugins: [Autoprefixer()],
     },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    base: env.ASSETS_BASE,
+    build: {
+      outDir: env.OUT_DIR || './dist',
     },
-  },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  }
 })
